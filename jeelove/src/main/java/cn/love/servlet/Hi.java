a11a27be8eb86485f63b251fbbd809b398124e5c
loveworldlovesky <zhouwenchao8426@126.com>
2017-05-02 13:39:48 +0800

    jeelove start at 2017-05-02

---
+++ jeelove/src/main/java/cn/love/servlet/Hi.java
@@ -0,0 +1,24 @@
+package cn.love.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Hi")
+public class Hi extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public Hi() {
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("haha 哈哈啊");
+	}
+
+}
